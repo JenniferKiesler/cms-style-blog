a1e79635a09370ccb666c2139c8b045757f2c1cf
JKies <jennyhawes24@gmail.com>
2022-12-04 18:00:07 -0600

    adds login view and route and starts dashboard view

--- controllers/view-routes.js
+++ controllers/view-routes.js
@@ -7,6 +7,7 @@ router.get('/', async (req, res) => {
             include: User
         })
         blogs = blogs.map(blog => blog.get({plain: true}))
+        console.log(req.originalUrl)
         res.render('home', {
             blogs,
             logged_in: req.session.logged_in
@@ -15,5 +16,28 @@ router.get('/', async (req, res) => {
         res.status(500).json(err)
     }
 })
+
+// router.get('/dashboard', async (req, res) => {
+//     try {
+//         let blogs = await Blog.findOne({
+//             where: {
+//                 username: req.session.user_id
+//             },
+//             include: User
+//         })
+//         blogs = blogs.map(blog => blog.get({plain: true}))
+//         console.log(blogs)
+//         res.render('dashboard', {
+//             blogs,
+//             logged_in: req.session.logged_in
+//         })
+//     } catch(err) {
+//         res.status(500).json(err)
+//     }
+// })
+
+router.get('/login', (req, res) => {
+    res.render('login')
+  })
 
 module.exports = router
