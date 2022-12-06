4a940f3af2d8387e4c7ec1e3640e5cd9217e6381
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-12-06 00:23:16 -0600

    adds edit post

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,9 +1,19 @@
 const newPost = document.getElementById('newPost')
 const createCard = document.getElementById('createPost')
 const createBlog = document.getElementById('createBlog')
+const exitPost = document.getElementById('exitCreate')
+const blogPostsContainer = document.getElementById('blogPosts')
+const editPost = document.getElementById('editPost')
+const fillTitle = document.getElementById('title')
+const fillContent = document.getElementById('content')
+const exitEdit = document.getElementById('exitEdit')
 
 newPost.addEventListener('click', (event) => {
   createCard.setAttribute('class', 'card my-3 visible')
+})
+
+exitPost.addEventListener('click', (event) => {
+  createCard.setAttribute('class', 'card my-3 invisible')
 })
 
 createBlog.addEventListener('submit', (event) => {
@@ -33,4 +43,19 @@ createBlog.addEventListener('submit', (event) => {
     }
   })
   .catch(err => console.log(err))
+})
+
+blogPostsContainer.addEventListener('click', (event) => {
+  if (event.target.matches('a')) {
+    editPost.setAttribute('class', 'card my-3 w-100 position-absolute top-50 start-50 translate-middle visible')
+
+    const titleValue = event.target.childNodes[1].innerHTML
+    fillTitle.setAttribute('value', titleValue)
+    const contentValue = event.target.childNodes[3].innerHTML
+    fillContent.textContent = contentValue
+  }
+})
+
+exitEdit.addEventListener('click', (event) => {
+  editPost.setAttribute('class', 'card my-3 w-100 position-absolute top-50 start-50 translate-middle invisible')
 })
